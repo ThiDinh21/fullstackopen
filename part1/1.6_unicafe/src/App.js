271ce0564b5b5ea341c6497a7ff8f18362bbc598
Thi Dinh <vietthidinh2001@gmail.com>
2021-10-15 02:34:49 +0300

    fix: 1.6

--- part1/1.6_unicafe/src/App.js
+++ part1/1.6_unicafe/src/App.js
@@ -1,25 +1,18 @@
 import React, { useState } from "react";
 
-const App = () => {
-	// save clicks of each button to its own state
-	const [good, setGood] = useState(0);
-	const [neutral, setNeutral] = useState(0);
-	const [bad, setBad] = useState(0);
+const Button = ({ text, handler }) => (
+	<button onClick={handler}>{text}</button>
+);
 
-	return (
-		<div>
-			<h1>give feedback</h1>
-			<RateButton text="good" handler={() => setGood(good + 1)} />
-			<RateButton
-				text="neutral"
-				handler={() => setNeutral(neutral + 1)}
-			/>
-			<RateButton text="bad" handler={() => setBad(bad + 1)} />
-			<h1>statistics</h1>
-			<Statistics nums={{ good: good, neutral: neutral, bad: bad }} />
-		</div>
-	);
-};
+const StatisticLine = ({ text, num, afterText }) => (
+	<tr>
+		<td>{text}</td>
+		<td>
+			{num}
+			{afterText}
+		</td>
+	</tr>
+);
 
 const Statistics = ({ nums }) => {
 	const { good, neutral, bad } = nums;
@@ -37,32 +30,39 @@ const Statistics = ({ nums }) => {
 
 	return (
 		<table>
-      <tbody>
-			<StatisticLine text="good" num={good} />
-			<StatisticLine text="neutral" num={neutral} />
-			<StatisticLine text="bad" num={bad} />
-			<StatisticLine text="average" num={getAvgRating()} />
-			<StatisticLine
-				text="positive"
-				num={getPositiveRating()}
-				afterText="%"
-			/>
-      </tbody>
+			<tbody>
+				<StatisticLine text="good" num={good} />
+				<StatisticLine text="neutral" num={neutral} />
+				<StatisticLine text="bad" num={bad} />
+				<StatisticLine text="average" num={getAvgRating()} />
+				<StatisticLine
+					text="positive"
+					num={getPositiveRating()}
+					afterText="%"
+				/>
+			</tbody>
 		</table>
 	);
 };
 
-const RateButton = ({ text, handler }) => (
-	<button onClick={handler}>{text}</button>
-);
+const App = () => {
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
 
-const StatisticLine = ({ text, num, afterText }) => (
-	<tr>
-		<td>{text}</td>
-		<td>
-			{num}{afterText}
-		</td>
-	</tr>
-);
+	return (
+		<div>
+			<h1>give feedback</h1>
+			<Button text="good" handler={() => setGood(good + 1)} />
+			<Button
+				text="neutral"
+				handler={() => setNeutral(neutral + 1)}
+			/>
+			<Button text="bad" handler={() => setBad(bad + 1)} />
+			<h1>statistics</h1>
+			<Statistics nums={{ good: good, neutral: neutral, bad: bad }} />
+		</div>
+	);
+};
 
 export default App;
