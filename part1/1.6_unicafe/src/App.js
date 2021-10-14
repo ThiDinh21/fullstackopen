f0656f0ac2690faaa949d223f956c73228980841
Thi Dinh <vietthidinh2001@gmail.com>
2021-10-15 02:01:40 +0300

    ex: 1.7

--- part1/1.6_unicafe/src/App.js
+++ part1/1.6_unicafe/src/App.js
@@ -1,27 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+	// save clicks of each button to its own state
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
 
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <RateButton text="good" handler={() => setGood(good + 1)} />
-      <RateButton text="neutral" handler={() => setNeutral(neutral + 1)} />
-      <RateButton text="bad" handler={() => setBad(bad + 1)} />
-      <h1>statistics</h1>
-      <RateResult text="good" num={good} />
-      <RateResult text="neutral" num={neutral} />
-      <RateResult text="bad" num={bad} />
-    </div>
-  )
-}
+	const getAvgRating = () => {
+		if (good === 0 && neutral === 0 && bad === 0) return 0;
+		return (good - bad) / (good + bad + neutral);
+	};
 
-const RateButton = ({text, handler}) => <button onClick={handler}>{text}</button>
+	const getPositiveRating = () => {
+		if (good === 0 && neutral === 0 && bad === 0) return 0;
+		return good / (good + bad + neutral);
+	};
 
-const RateResult = ({text, num}) => <p>{text} {num}</p>
+	return (
+		<div>
+			<h1>give feedback</h1>
+			<RateButton text="good" handler={() => setGood(good + 1)} />
+			<RateButton
+				text="neutral"
+				handler={() => setNeutral(neutral + 1)}
+			/>
+			<RateButton text="bad" handler={() => setBad(bad + 1)} />
+			<h1>statistics</h1>
+			<RateResult text="good" num={good} />
+			<RateResult text="neutral" num={neutral} />
+			<RateResult text="bad" num={bad} />
+			<RateResult text="average" num={getAvgRating()} />
+			<RateResult
+				text="positive"
+				num={getPositiveRating()}
+				afterText="%"
+			/>
+		</div>
+	);
+};
 
-export default App
+const RateButton = ({ text, handler }) => (
+	<button onClick={handler}>{text}</button>
+);
+
+const RateResult = ({ text, num, afterText }) => (
+	<p>
+		{text} {num} {afterText}
+	</p>
+);
+
+export default App;
