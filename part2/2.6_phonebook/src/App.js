22e3c0d09cb3b1e738150a5e23f7d6ade67ec551
Thi Dinh <vietthidinh2001@gmail.com>
2021-10-21 01:14:37 +0300

    ex: 2.15-2.20

--- part2/2.6_phonebook/src/App.js
+++ part2/2.6_phonebook/src/App.js
@@ -1,14 +1,16 @@
-import axios from "axios";
 import React, { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import phonebookService from "./services/persons";
 
 const App = () => {
 	const [persons, setPersons] = useState([]);
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
 	const [searchTerm, setSearchTerm] = useState("");
+	const [msg, setMsg] = useState(null);
+	const [err, setErr] = useState(null);
 
 	const onChangeName = (event) => {
 		setNewName(event.target.value);
@@ -22,28 +24,57 @@ const App = () => {
 		setSearchTerm(event.target.value);
 	};
 
-	const hook = () => {
-		axios
-			.get("http://localhost:3001/persons")
-			.then((resp) => setPersons(resp.data));
+	const fetchAll = () => {
+		phonebookService.getAll().then((data) => setPersons(data));
 	};
 
-	useEffect(hook, []);
+	useEffect(fetchAll, []);
 
 	const onSubmit = (event) => {
 		event.preventDefault();
-		if (persons.findIndex((person) => person.name === newName) !== -1) {
-			window.alert(`${newName} is already existed`);
-			return;
-		}
 		const newPerson = {
-			id: persons.length,
 			name: newName,
 			number: newNumber,
 		};
-		setPersons(persons.concat(newPerson));
+		if (persons.findIndex((person) => person.name === newName) !== -1) {
+			const res = window.confirm(
+				`${newName} is already existed, update?`
+			);
+			const id = persons.find((p) => p.name === newName).id;
+			if (res) {
+				updatePerson(id, newPerson);
+			}
+		} else {
+			phonebookService
+				.create(newPerson)
+				.then((data) => setPersons(persons.concat(data)));
+			setMsg(`Added ${newName}`);
+		}
+
 		setNewName("");
 		setNewNumber("");
+		setTimeout(() => setMsg(null), 5000);
+	};
+
+	const updatePerson = (id, newObj) => {
+		phonebookService
+			.update(id, newObj)
+			.then((data) => {
+				setPersons(persons.map((p) => (p.id === id ? data : p)));
+				setMsg(`Updated ${newName}`);
+			})
+			.catch((err) => {
+				setErr(`Info of ${newObj.name} had been removed from db`);
+				setTimeout(() => setErr(null), 5000);
+			});
+	};
+
+	const deletePerson = (id) => {
+		phonebookService.del(id).then(() => {
+			phonebookService.getAll().then((data) => {
+				setPersons(data);
+			});
+		});
 	};
 
 	const filteredPersons = persons.filter((person) =>
@@ -53,6 +84,9 @@ const App = () => {
 	return (
 		<div>
 			<h2>Phonebook</h2>
+			{msg == null ? null : <p className="msg">{msg}</p>}
+			{err == null ? null : <p className="error">{err}</p>}
+
 			<Filter searchTerm={searchTerm} onChange={onSearchChange} />
 			<h2>Add new</h2>
 			<PersonForm
@@ -63,7 +97,7 @@ const App = () => {
 				onChangeNumber={onChangeNumber}
 			/>
 			<h2>Numbers</h2>
-			<Persons persons={filteredPersons} />
+			<Persons persons={filteredPersons} deletePerson={deletePerson} />
 		</div>
 	);
 };
--- part2/2.6_phonebook/src/components/Persons.js
+++ part2/2.6_phonebook/src/components/Persons.js
@@ -1,12 +1,22 @@
-import React from "react";
-
-const Persons = ({ persons }) => {
+const Persons = ({ persons, deletePerson }) => {
 	return (
 		<div>
 			{persons.map((person) => (
-				<p key={person.id}>
-					{person.name} --- {person.number}
-				</p>
+				<div key={person.id}>
+					<span>
+						{person.name} --- {person.number}
+					</span>
+					<button
+						onClick={() => {
+							const result = window.confirm(
+								`Delete ${person.name}?`
+							);
+							if (result) deletePerson(person.id);
+						}}
+					>
+						Delete
+					</button>
+				</div>
 			))}
 		</div>
 	);
