581b7537b12c3088139a95da932646162e184156
Thi Dinh <vietthidinh2001@gmail.com>
2021-11-13 00:04:35 +0200

    ex: 9.6 - 9.7

--- part9/bmi/exerciseCalculator.ts
+++ part9/bmi/exerciseCalculator.ts
@@ -1,4 +1,4 @@
-interface Result {
+export interface ExerciseResult {
 	periodLength: number;
 	trainingDays: number;
 	target: number;
@@ -8,16 +8,18 @@ interface Result {
 	ratingDescription: "Bad" | "Ok" | "Good";
 }
 
-interface Input2 {
-	data: number[];
+export interface ExerciseInput {
+	daily_exercises: number[];
+	target: number;
 }
 
-const calculateExercises = (trainingData: number[]): Result => {
+export const calculateExercises = (input: ExerciseInput): ExerciseResult => {
+	const trainingData = input.daily_exercises;
 	const days = trainingData.length;
 	const trainingDays = trainingData.filter((hour) => hour > 0).length;
-	const target = 2;
+	const target = input.target;
 	const avg = trainingData.reduce((prev, curr) => prev + curr) / days;
-	let success: boolean = false;
+	let success = false;
 	let rating: 1 | 2 | 3;
 	let ratingDescription: "Bad" | "Ok" | "Good";
 	if (avg >= target) {
@@ -32,7 +34,7 @@ const calculateExercises = (trainingData: number[]): Result => {
 		ratingDescription = "Bad";
 	}
 
-	const res: Result = {
+	const res: ExerciseResult = {
 		periodLength: days,
 		trainingDays: trainingDays,
 		target: target,
@@ -44,30 +46,3 @@ const calculateExercises = (trainingData: number[]): Result => {
 
 	return res;
 };
-
-const parseArguments2 = (args: string[]): Input2 => {
-	if (args.length < 3) throw new Error("Not enough arguments");
-
-	let input: number[] = [];
-
-	for (let i = 2; i < args.length; i++) {
-		if (isNaN(Number(args[i])))
-			throw new Error("Provided values were not numbers!");
-		input.push(Number(args[i]));
-	}
-
-	return {
-		data: input,
-	};
-};
-
-try {
-	const input = parseArguments2(process.argv);
-	console.log(calculateExercises(input.data));
-} catch (error: unknown) {
-	let errorMessage = "Something bad happened.";
-	if (error instanceof Error) {
-		errorMessage += " Error: " + error.message;
-	}
-	console.log(errorMessage);
-}
--- part9/bmi/index.ts
+++ part9/bmi/index.ts
@@ -1,8 +1,11 @@
-import express from "express";
+import express, { json } from "express";
 import { validateInput, calculateBmi } from "./bmiCalculator";
+import { calculateExercises, ExerciseInput } from "./exerciseCalculator";
 
 const app = express();
 const PORT = 3002;
+
+app.use(json());
 
 app.get("/ping", (_req, resp) => {
 	resp.send("pong");
@@ -16,6 +19,23 @@ app.get("/bmi", (req, resp) => {
 		resp.json(calculateBmi(h, w));
 	} else {
 		resp.statusCode = 404;
+		resp.json({
+			error: "malformatted parameters",
+		});
+	}
+});
+
+app.post("/exercises", (req, resp) => {
+	const input = req.body as ExerciseInput;
+
+	if (
+		!isNaN(input.target) &&
+		input.daily_exercises.every((x) => !isNaN(x)) &&
+		input.daily_exercises.length > 1
+	) {
+		resp.json(calculateExercises(input));
+	} else {
+		resp.statusCode = 400;
 		resp.json({
 			error: "malformatted parameters",
 		});
